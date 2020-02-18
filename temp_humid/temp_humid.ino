637e736d71bd42b1ec9f2fe2b7901bc050f57057
Kai Timmer <kai@staffbase.com>
2020-02-18 16:00:40 +0100

    First working version on NodeMCU

---
+++ temp_humid/temp_humid.ino
@@ -0,0 +1,87 @@
+#include <DHTesp.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+#include <PubSubClient.h>
+#include "config.h"
+
+DHTesp dht;
+
+WiFiClientSecure wifiClientSecure;
+
+
+PubSubClient client(wifiClientSecure);
+
+void setup()
+{
+  Serial.begin(74880);
+  Serial.println("Started..");
+
+  dht.setup(14, DHTesp::DHT22);
+  
+  // Accept all Certificates on the MQTT Broker
+  wifiClientSecure.setInsecure();
+  wifiConnect();
+
+  // connect to mqtt server
+  client.setServer(mqttServer, mqttPort);
+  mqttReconnect();
+}
+
+void wifiConnect()
+{
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, wifiPass);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting to WiFi..");
+  }
+  Serial.println("Connected to the WiFi network");
+  // Print the IP address
+  Serial.println(WiFi.localIP());
+}
+
+void mqttReconnect()
+{
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect(clientId, mqttUser, mqttPassword)) {
+      Serial.println("connected");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 2.5 seconds");
+      delay(2500);
+    }
+  }
+}
+
+void loop()
+{
+  char sendBuf[128];
+  
+  client.loop();
+
+  if (!client.connected())
+  {
+    mqttReconnect();
+  }
+
+  delay(dht.getMinimumSamplingPeriod());
+  
+  TempAndHumidity measurement = dht.getTempAndHumidity();
+ 
+  Serial.print("Temperature: ");
+  Serial.println(measurement.temperature);
+ 
+  Serial.print("Humidity: ");
+  Serial.println(measurement.humidity);
+
+  sprintf(sendBuf, "TempAndHumidity,sensor=DHT22 temperature=%.2f,humidity=%.2f", measurement.temperature, measurement.humidity);
+
+  client.publish(mqttTopic, sendBuf);
+  
+  delay(interval);
+}
